a6506d6b1c7877699566fe8498fad6cc989035a3
pjsm <pjsm.lesi@gmail.com>
2025-03-22 18:52:24 +0000

    CREATED: Lab2.java, UPDATED: notes

---
+++ Module2/PrimitiveDataTypes/Lab2.java
@@ -0,0 +1,52 @@
+public class Lab2 {
+    public static void main(String[] args) {
+        int width;
+        int height;
+        int area;
+        double radius = 10.0;
+        double pi = 3.14;
+        boolean result = true;
+
+        int[] daysInMonths = new int[12];
+        String[] monthNames = {
+                "January", "February", "March",
+                "April", "May", "June", "July",
+                "August", "September", "October",
+                "November", "December"
+        };
+
+        width = 8;
+        height = 12;
+        area = 96;
+
+        daysInMonths[0] = 31;
+        daysInMonths[1] = 28;
+        daysInMonths[2] = 31;
+        daysInMonths[3] = 30;
+        daysInMonths[4] = 31;
+        daysInMonths[5] = 30;
+        daysInMonths[6] = 31;
+        daysInMonths[7] = 31;
+        daysInMonths[8] = 30;
+        daysInMonths[9] = 31;
+        daysInMonths[10] = 30;
+        daysInMonths[11] = 31;
+
+        System.out.println("Hello World");
+        System.out.println("----------------");
+        System.out.println("The value of width is " + width + ".");
+        System.out.println("----------------");
+        System.out.println( monthNames[0] + " has " + daysInMonths[0] + " days.");
+        System.out.println( monthNames[1] + " has " + daysInMonths[1] + " days.");
+        System.out.println( monthNames[2] + " has " + daysInMonths[2] + " days.");
+        System.out.println( monthNames[3] + " has " + daysInMonths[3] + " days.");
+        System.out.println( monthNames[4] + " has " + daysInMonths[4] + " days.");
+        System.out.println( monthNames[5] + " has " + daysInMonths[5] + " days.");
+        System.out.println( monthNames[6] + " has " + daysInMonths[6] + " days.");
+        System.out.println( monthNames[7] + " has " + daysInMonths[7] + " days.");
+        System.out.println( monthNames[8] + " has " + daysInMonths[8] + " days.");
+        System.out.println( monthNames[9] + " has " + daysInMonths[9] + " days.");
+        System.out.println( monthNames[10] + " has " + daysInMonths[10] + " days.");
+        System.out.println( monthNames[11] + " has " + daysInMonths[11] + " days.");
+    }
+}
