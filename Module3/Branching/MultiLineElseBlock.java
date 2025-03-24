dd5fc9a1a28eaf0ac07484cdc93fc5544f1bd590
pjsm <pjsm.lesi@gmail.com>
2025-03-24 18:11:07 +0000

    Created: MultiLineElseBlock.java

--- Module3/Branching/MultiLineElseBlock.java
+++ Module3/Branching/MultiLineElseBlock.java
@@ -1,6 +1,7 @@
 public class MultiLineElseBlock {
     public static void main(String[] args) {
         int x = 12;
+        int y, z;
 
         if (x < 7) {
             System.out.println("X is less than 7.");
