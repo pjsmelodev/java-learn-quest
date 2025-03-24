14784381ba549e779ebebd94bb9fe57867c80ecc
pjsm <pjsm.lesi@gmail.com>
2025-03-24 18:21:49 +0000

    Created: SwitchExample.java

---
+++ Module3/Branching/SwitchExample.java
@@ -0,0 +1,23 @@
+public class SwitchExample {
+    public static void main(String[] args) {
+        int wattage = 10;
+        double price;
+
+        switch (wattage) {
+            case 40:
+                price = 1.20;
+                break;
+            case 60:
+                price = 1.30;
+                break;
+            case 100:   // no break, will always be processed
+            case 150:
+                price = 1.15;
+                break;
+            default:
+                price = 0;
+
+        System.out.println(price);
+        }
+    }
+}
