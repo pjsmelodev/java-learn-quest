68588ad3620a7909b13d00d2a87672aaa16cc58a
pjsm <pjsm.lesi@gmail.com>
2025-03-24 19:29:00 +0000

    Created: DoWhileLoop.java

---
+++ Module3/Looping/DoWhileLoop.java
@@ -0,0 +1,12 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int count = 20;
+        int n = 20;
+
+        do {
+            System.out.print("DW");
+            count++;
+        }
+        while (count < n);
+    }
+}
