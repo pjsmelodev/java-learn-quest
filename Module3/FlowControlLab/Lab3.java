b3483fdf74dfcee78e43c40dc3d47a61ff9865c5
pjsm <pjsm.lesi@gmail.com>
2025-03-24 23:13:49 +0000

    Updated: Lab3.java

--- Module3/FlowControlLab/Lab3.java
+++ Module3/FlowControlLab/Lab3.java
@@ -1,5 +1,12 @@
 public class Lab3 {
     public static void main(String[] args) {
+        // 11.
+        String[] monthNames = {
+                "January", "February", "March", "April",
+                "May", "June", "July", "August",
+                "September", "October", "November", "December"
+        };
+
         // Exercise 1
 
         // 2.
@@ -47,6 +54,76 @@ public class Lab3 {
 
         // Exercise 3:
 
+        // 7.
+        for (int j = 1; j <= 100; j++) {
+            if (j == 50 || j == 60) {
+                continue;
+            }
+            else {
+                System.out.println(j);
+            }
+        }
+
+        // Exercise 4:
+
+        // 8.
+        int loopCounter = 1;
+        while (loopCounter < 12) {
+            // 9, 11.
+            String month = monthNames[loopCounter];
+
+            switch (month) {
+                // 10.
+                case "January":
+                case "March":
+                case "May":
+                case "July":
+                case "August":
+                case "October":
+                case "December":
+                    System.out.println(month + " has 31 days.");
+                    break;
+                case "February":
+                    System.out.println(month + " has 28 days.");
+                    break;
+                case "April":
+                case "June":
+                case "September":
+                case "November":
+                    System.out.println(month + " has 30 days.");
+                    break;
+            }
+            loopCounter++;
+        }
+
+        System.out.println();
+
+        // Challenge Exercise
+
+        String[] weekDays = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
+        int daysInMarch = 31;
+        int startDay = 5;
+
+        for (String day : weekDays) {
+            System.out.print(day + "\t");
+        }
+        System.out.println();
+
+        int currentDay = 0;
+        for (int k = 0; k < startDay; k++) {
+            System.out.print("\t");
+            currentDay++;
+        }
+
+        for (int day = 1; day <= daysInMarch; day++) {
+            System.out.print(day + "\t");
+            currentDay++;
+            if (currentDay % 7 == 0) {
+                System.out.println();
+            }
+        }
+        System.out.println();
 
     }
 }
