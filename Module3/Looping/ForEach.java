1294b1826a5b780a1514c7abb80772da680c5115
pjsm <pjsm.lesi@gmail.com>
2025-03-24 19:47:12 +0000

    Updated: ForEach.java

--- Module3/Looping/ForEach.java
+++ Module3/Looping/ForEach.java
@@ -3,7 +3,7 @@ public class ForEach {
         String[] sa = {"o1", "o2", "o3"};
 
         for (String s : sa) {
-            System.out.println(s);
+            System.out.println(s);  //s.ToString() -> redundante (já é string)
         }
 
         System.out.println();
@@ -11,7 +11,7 @@ public class ForEach {
         Integer[] ia = {1, 2, 3};
 
         for (Integer i2 : ia) {
-            System.out.println(i2);
+            System.out.println(i2); //i2.IntValue() -> redundante (autounboxing)
         }
     }
 }
