823ae346819a7aaf20961c273c96a6cb4debe67f
pjsm <pjsm.lesi@gmail.com>
2025-03-24 19:23:36 +0000

    Created: WhileLoop.java

---
+++ Module3/Looping/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        int n = 20;
+
+        while (count < n) {
+            System.out.println("*");
+            count++;
+        }
+    }
+}
