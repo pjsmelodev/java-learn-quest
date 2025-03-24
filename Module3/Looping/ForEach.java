a1a262c8031a64144657abb9528d69366955c071
pjsm <pjsm.lesi@gmail.com>
2025-03-24 19:44:38 +0000

    Created: ForEach.java

---
+++ Module3/Looping/ForEach.java
@@ -0,0 +1,17 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] sa = {"o1", "o2", "o3"};
+
+        for (String s : sa) {
+            System.out.println(s.toString());
+        }
+
+        System.out.println();
+
+        Integer[] ia = {1, 2, 3};
+
+        for (Integer i2 : ia) {
+            System.out.println(i2);
+        }
+    }
+}
